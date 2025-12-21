fc0e06b168502306f951064a105535772ee001d9
Vedang Manerikar <ved.manerikar@gmail.com>
2025-12-21 15:18:05 +0530

    chore: bump version number to v1.1.0

--- alabaster-mono-theme.el
+++ alabaster-mono-theme.el
@@ -5,7 +5,7 @@
 ;; Author: Nikita Prokopov
 ;; Maintainer: Vedang Manerikar
 ;; URL: https://github.com/vedang/alabaster-themes
-;; Version: 1.0.1
+;; Version: 1.1.0
 ;; Package-Requires: ((emacs "28.1"))
 ;; Keywords: faces, theme, minimal
 
